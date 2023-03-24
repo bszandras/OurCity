283e1773460a994b8308295e0baebf9cdcb472be
Bogár-Szabó András <i2hvec@inf.elte.hu>
2023-03-24 18:17:55 +0100

    Vsync kapcsoló main.cpp-be

--- OurCity/main.cpp
+++ OurCity/main.cpp
@@ -3,6 +3,7 @@
 int main( int argc, char* args[] )
 {
 	// width, height, vsync(0 vagy 1)
-	Window* window = new Window(600,600,1);
+	int vsync = 1;
+	Window* window = new Window(600,600,vsync);
 	return 0;
 }
