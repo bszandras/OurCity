506ebf15739308f5bdd71774eb4dd6a556cf5428
aroland03 <jbay66@inf.elte.hu>
2023-03-28 12:19:03 +0200

    Próba a brencsen

--- OurCity/Tile.h
+++ OurCity/Tile.h
@@ -5,6 +5,9 @@ struct Tile
 	// FONTOS, hogy ezek koordináták nem indexek!
 	int x;
 	int y;
+
+	int i;
+	int j;
 	// egyéb infók ...
 	// building pointer pl
 	// saját grafikai rect-jét nem tárolja el, saját koordinátái alapján vissza lehet fejteni
