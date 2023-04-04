0455ced8bdc42d2a40688012c59c0fa31a29e965
aroland03 <jbay66@inf.elte.hu>
2023-04-04 18:31:39 +0200

    BuildingInclude frissítése

--- OurCity/BuildingsInclude.h
+++ OurCity/BuildingsInclude.h
@@ -2,7 +2,14 @@
 #define BUILDINGSINCLUDE_H
 
 #include "Factory.h"
-#include "House.h"
 #include "ServiceBuilding.h"
+#include "House.h"
+#include "FireStation.h"
+#include "Stadium.h"
+#include "Forest.h"
+#include "HighSchool.h"
+#include "University.h"
+#include "PoliceStation.h"
+#include "Road.h"
 
 #endif
