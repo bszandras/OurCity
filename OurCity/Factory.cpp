796ade0cc9b101dde37a3cbb3233cc6091c106a7
Réka Samu <h0izyc@INF.ELTE.HU>
2023-05-01 16:52:41 +0200

    Ipar negatív hatása fv-k

--- OurCity/Factory.cpp
+++ OurCity/Factory.cpp
@@ -9,4 +9,44 @@ Factory::Factory(Tile* tile) : Workplace(tile)
 	this->tile = tile;
 	this->capacity = 8;
 	this->tax = 0;
+	this->range = 0; //?
+
+}
+
+void Factory::setRange(int r)
+{
+	this->range = r;
+}
+
+void Factory::setRangeTiles(std::vector<int> tiles)
+{
+	this->rangeTiles = tiles;
+}
+
+void Factory::UpdateAreaAfterBuilding(Tile** tiles, int tileCount)
+{
+	for (int i = 0; i < tileCount; i++)
+	{
+		tiles[i]->happinessModifer -= minusHappiness;
+		updatedTiles.push_back(tiles[i]);
+	}
+}
+
+void Factory::UpdateAreaAfterDestruction()
+{
+	for (int i = 0; i < updatedTiles.size(); i++)
+	{
+		updatedTiles[i]->happinessModifer += minusHappiness;
+	}
+	updatedTiles.clear();
+}
+
+int Factory::getRange() const
+{
+	return this->range;
+}
+
+std::vector<int> Factory::getRangeTiles() const
+{
+	return this->rangeTiles;
 }
--- OurCity/Factory.h
+++ OurCity/Factory.h
@@ -5,8 +5,20 @@
 
 class Factory : public Workplace
 {
+private:
+	int range;
+	std::vector<int> rangeTiles;
+	std::vector<Tile*> updatedTiles;
+	int minusHappiness;
 public:
 	Factory(Tile* tile);
+
+	void setRange(int r);
+	void setRangeTiles(std::vector<int> tiles);
+	void UpdateAreaAfterBuilding(Tile** tiles, int tileCount);
+	void UpdateAreaAfterDestruction();
+	int getRange() const;
+	std::vector<int> getRangeTiles() const;
 };
 
 #endif
